d0cfede6bab7da29ccab41557b38af125bf6c186
avduryagin <avduryagin@mail.ru>
2022-05-21 00:27:31 +0300

    Приведены типы. Отредактирован метод вычисления repairs_map

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 #import joblib
 import pickle
 
@@ -154,3 +155,139 @@ class Generator:
         x[self.col['nivl0']:self.col['nivl5'] + 1]=np.cumsum(arr)
 
         return q
+
+
+def get_jd_counts(data):
+    columns = ['new_id', 'L', 'D', 'interval', 'prevent', 'lost', 'length']
+    frame = []
+    aggdata = data.groupby('new_id')
+    for i, groups in enumerate(aggdata):
+        group = groups[1]
+        # raw=cdata[cdata['new_id']==groups[0]]
+        # print(group.shape)
+        # print(group['interval'].value_counts().keys())
+        for interval in group['interval'].value_counts().keys():
+            subgroup = group[group['interval'] == interval]
+            array = []
+            array.append(groups[0])
+            # array.append(groups[0])
+            array.append(subgroup['L,м'].iloc[0])
+            array.append(subgroup['D'].iloc[0])
+            array.append(interval)
+            array.append(subgroup['prevent'].sum())
+            array.append(subgroup['lost'].sum())
+
+            lenght = 0
+            for j in subgroup['interseption'].value_counts().keys():
+                interseption = subgroup[subgroup['interseption'] == j]
+                lenght = lenght + interseption.iloc[0]['joined_length']
+            array.append(lenght)
+            frame.append(array)
+    return pd.DataFrame(data=frame, columns=columns)
+    # mask=pd.Series(index=raw.index,data=np.ones(raw.shape[0],dtype=bool))
+    # mask=forward(data,raw,mask=mask,indices=subgroup.index)
+    # mask=backward(data,raw,mask=mask,indices=subgroup.index
+
+
+def set_fb(data, cdata):
+    aggdata = data.groupby('new_id')
+    for i, groups in enumerate(aggdata):
+        group = groups[1]
+        raw = cdata[cdata['new_id'] == groups[0]]
+        # print(group.shape)
+        # print(group['interval'].value_counts().keys())
+        for interval in group['interval'].value_counts().keys():
+            subgroup = group[group['interval'] == interval]
+            mask = pd.Series(index=raw.index, data=np.ones(raw.shape[0], dtype=bool))
+            mask = forward(data, raw, mask=mask, indices=subgroup.index)
+            mask = backward(data, raw, mask=mask, indices=subgroup.index)
+
+
+def set_reverse(data=pd.DataFrame([])):
+    aggdata = data.groupby('new_id')
+    for i, groups in enumerate(aggdata):
+        group = groups[1]
+        # raw=cdata[cdata['new_id']==groups[0]]
+        # print(group.shape)
+        # print(group['interval'].value_counts().keys())
+        for interval in group['interval'].value_counts().keys():
+            subgroup = group[group['interval'] == interval]
+            indices = subgroup.index
+            reverse(data, indices=indices)
+
+
+def reverse(data, indices=np.array([], dtype=int)):
+    group = data.loc[indices]
+    j = -1
+    while j >= -indices.shape[0]:
+        i = indices[j]
+        j = j - 1
+        if data.loc[i, 'base'] == True:
+            a = group.loc[i, 'lbound']
+            b = group.loc[i, 'rbound']
+            date = group.loc[i, 'Дата аварии']
+            mask1 = (group['addres'] >= a) & (group['addres'] <= b)
+            mask2 = group['Дата аварии'] < date
+            # data.loc[i,'base']=True
+            mask3 = mask1 & mask2
+            index = mask3[mask3 == True].index
+            data.loc[index, 'base'] = False
+
+
+def forward(data, cdata, mask=pd.Series(data=np.array([], dtype=bool)), indices=np.array([], dtype=int)):
+    for i in indices:
+        if data.loc[i, 'prediction'] == 1:
+            a = data.loc[i, 'lbound']
+            b = data.loc[i, 'rbound']
+            f, pr, pr_y, msk = get_counters(cdata, index=data.loc[i, 'oi'], a=a, b=b, mask=mask)
+            mask = msk
+            # print(mask[mask==False].shape)
+            data.loc[i, 'prevent'] = f
+    return mask
+
+
+def backward(data, cdata, mask=pd.Series(data=np.array([], dtype=bool)), indices=np.array([], dtype=int)):
+    for i in indices:
+        if data.loc[i, 'prediction'] == 0:
+            a = data.loc[i, 'lbound']
+            b = data.loc[i, 'rbound']
+            f, pr, pr_y, msk = get_counters(cdata, index=data.loc[i, 'oi'], a=a, b=b, mask=mask)
+            mask = msk
+            data.loc[i, 'lost'] = f
+    return mask
+
+
+def get_counters(group, index, date=3, year=2017, mask=pd.Series(data=np.array([], dtype=bool)), a=0, b=0):
+    # a=group.loc[index,'lbound']
+    # b=group.loc[index,'rbound']
+    mask1 = (group['Адрес от начала участка (new)'] >= a) & (group['Адрес от начала участка (new)'] <= b)
+    current = group.loc[index, 'Дата аварии']
+    date_out = group.loc[index, 'Дата перевода в бездействие']
+    current_age = group.loc[index, 'Наработка до отказа(new), лет']
+    getin = group.loc[index, 'Дата ввода']
+    get_out = (date_out - getin) / np.timedelta64(1, 'Y')
+    mask3 = group['Дата аварии'] > current
+    mask4 = group['Дата аварии'] <= current
+    mask5 = group['Дата аварии'].dt.year < year
+    fimask = mask1 & mask3
+    future_ads = group[mask1 & mask3]
+    future_ads['delta'] = (future_ads['Дата аварии'] - current) / np.timedelta64(1, 'Y')
+    previous = group[mask1 & mask4]
+    previous_y = group[mask1 & mask5]
+    previous_count = previous.shape[0]
+    previous_y_count = previous_y.shape[0]
+    # data.loc[indices,'previous_y']=previous_y.shape[0]
+    mask6 = future_ads['delta'] <= date
+    marked = mask6[mask6 == True].index
+    if get_out >= (current_age + date):
+        future_ads_y = future_ads[mask & mask6]
+        future = future_ads_y.shape[0]
+    else:
+        we_future_ads_y = future_ads[mask & mask6]
+        if we_future_ads_y.shape[0] > 0:
+            future = we_future_ads_y.shape[0]
+        else:
+            future = np.nan
+    mask.loc[marked] = False
+    return future, previous_count, previous_y_count, mask
+
